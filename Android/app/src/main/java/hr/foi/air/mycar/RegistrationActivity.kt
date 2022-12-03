1b413640e4ac75acb5d3811fed84feec29ea3007
DenisMatijevic0901 <pine.ananasaple@gmail.com>
2022-12-03 22:02:31 +0100

    Izrada dizajna za funkcionalnost registracije

---
+++ Android/app/src/main/java/hr/foi/air/mycar/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package hr.foi.air.mycar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+    }
+}
