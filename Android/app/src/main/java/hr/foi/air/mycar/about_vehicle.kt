d2e629e62a5b305bc769683a4c64237c03188761
Forky10911 <95697055+Forky10911@users.noreply.github.com>
2022-12-04 14:04:19 +0100

    Izrada UI za activity about_vehicle (o vozilu).

---
+++ Android/app/src/main/java/hr/foi/air/mycar/about_vehicle.kt
@@ -0,0 +1,11 @@
+package hr.foi.air.mycar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class about_vehicle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_vehicle)
+    }
+}
