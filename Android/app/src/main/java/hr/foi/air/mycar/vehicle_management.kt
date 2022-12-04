02f04f04eba2072e59c1a2904227dcd29dde95d8
Forky10911 <95697055+Forky10911@users.noreply.github.com>
2022-12-04 02:52:33 +0100

    Vehicle_management UI
    Izrada main activity UI. Izrada vehicle_management funkcionalnosti UI,
    djelomicno izradeno.

--- Android/app/src/main/java/hr/foi/air/mycar/vehicle_management.kt
+++ Android/app/src/main/java/hr/foi/air/mycar/vehicle_management.kt
@@ -26,7 +26,7 @@ class vehicle_management : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.myCar, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
