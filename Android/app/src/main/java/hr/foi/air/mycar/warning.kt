0d23dcbc67a40fc4d5a1fc35e84b62db74d51987
Forky10911 <95697055+Forky10911@users.noreply.github.com>
2022-12-04 13:25:14 +0100

    Izrada UI za activity warnings (upozorenja).

---
+++ Android/app/src/main/java/hr/foi/air/mycar/warning.kt
@@ -0,0 +1,11 @@
+package hr.foi.air.mycar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class warning : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_warning)
+    }
+}
