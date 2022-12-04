cea4e4e05f46fc48c712cda68d151ca127fb789f
DenisMatijevic0901 <pine.ananasaple@gmail.com>
2022-12-04 02:11:36 +0100

    Update dizajna za funkcionalnost registracije

--- Android/app/src/main/java/hr/foi/air/mycar/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/mycar/MainActivity.kt
@@ -1,9 +1,13 @@
 package hr.foi.air.mycar
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- Android/app/src/main/java/hr/foi/air/mycar/RegistrationActivity.kt
+++ Android/app/src/main/java/hr/foi/air/mycar/RegistrationActivity.kt
@@ -1,12 +1,33 @@
 package hr.foi.air.mycar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        //password show - on/off
+        val btnShowPassword = findViewById<Button>(R.id.btnShowPasswordRegistration)
+        var textPassword = findViewById<EditText>(R.id.inputPassword)
+        btnShowPassword.setOnClickListener {
+            if (textPassword.inputType != 1) {
+                textPassword.inputType = 1
+            } else if (textPassword.inputType == 1) {
+                textPassword.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+        }
+
+        val btnBack = findViewById<Button>(R.id.btnBackRegistration)
+        btnBack.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
