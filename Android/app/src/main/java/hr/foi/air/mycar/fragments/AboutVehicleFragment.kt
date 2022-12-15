308931d18a5b007797a5fb6f36ae3aeb12a7d727
Forky10911 <95697055+Forky10911@users.noreply.github.com>
2022-12-15 16:13:50 +0100

    Update navigacijske trake.

--- Android/app/src/main/java/hr/foi/air/mycar/fragments/AboutVehicleFragment.kt
+++ Android/app/src/main/java/hr/foi/air/mycar/fragments/AboutVehicleFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import hr.foi.air.mycar.R
 
 class AboutVehicleFragment : Fragment() {
@@ -18,6 +19,13 @@ class AboutVehicleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_vehicle, container, false)
+        val view = inflater.inflate(R.layout.fragment_about_vehicle, container, false)
+        val btnNazad : Button = view.findViewById(R.id.btn_back)
+        btnNazad.setOnClickListener{
+            val fragment = VehicleManagementFragment()
+            val transaction = childFragmentManager?.beginTransaction()
+            transaction?.replace(R.id.aboutVehicle_id,fragment)?.commit()
+        }
+        return view
     }
 }
--- Android/app/src/main/java/hr/foi/air/mycar/fragments/VehicleManagementFragment.kt
+++ Android/app/src/main/java/hr/foi/air/mycar/fragments/VehicleManagementFragment.kt
@@ -1,10 +1,13 @@
 package hr.foi.air.mycar.fragments
 
 import android.os.Bundle
+import android.telecom.Call.Details
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
 import hr.foi.air.mycar.R
 
 class VehicleManagementFragment : Fragment() {
@@ -18,6 +21,13 @@ class VehicleManagementFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vehicle_management, container, false)
+        val view = inflater.inflate(R.layout.fragment_vehicle_management, container, false)
+        val btnDetalji : Button = view.findViewById(R.id.btn_details)
+        btnDetalji.setOnClickListener{
+            val fragment = AboutVehicleFragment()
+            val transaction = childFragmentManager.beginTransaction()
+            transaction?.replace(R.id.fragment_vehicle_management_id, fragment)?.commit()
+        }
+        return view
     }
 }
