64c10610ef158e420b43387ac61cd48623e4575a
DenisMatijevic0901 <pine.ananasaple@gmail.com>
2022-12-20 21:52:47 +0100

    Implementacija funkcionalnosti registracije
    - povezivanje s bazom podataka
    - unos novog korisnika u bazu

--- Android/app/src/main/java/hr/foi/air/mycar/RegistrationActivity.kt
+++ Android/app/src/main/java/hr/foi/air/mycar/RegistrationActivity.kt
@@ -6,11 +6,34 @@ import android.os.Bundle
 import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.android.volley.*
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var inputImeIPrezime: EditText
+    private lateinit var inputEmail: EditText
+    private lateinit var inputKorisnickoIme: EditText
+    private lateinit var inputLozinka: EditText
+    private lateinit var btnRegistirajSe: Button
+    private lateinit var requestQueue: RequestQueue
+    private lateinit var url: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        inputImeIPrezime = findViewById(R.id.inputNameAndSurname)
+        inputEmail = findViewById(R.id.inputEmail)
+        inputKorisnickoIme = findViewById(R.id.inputUsername)
+        inputLozinka = findViewById(R.id.inputPassword)
+        btnRegistirajSe = findViewById(R.id.btnRegister)
+        requestQueue = Volley.newRequestQueue(applicationContext);
+        url = "https://mycar-db.000webhostapp.com/insertUser.php"
 
         //password show - on/off
         val btnShowPassword = findViewById<Button>(R.id.btnShowPasswordRegistration)
@@ -26,9 +49,43 @@ class RegistrationActivity : AppCompatActivity() {
 
         //povratak na Home fragment
         val btnBack = findViewById<Button>(R.id.btnBackRegistration)
-        btnBack.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        btnRegistirajSe.setOnClickListener {
+
+            if (inputImeIPrezime.text.toString() == "" || inputEmail.text.toString() == "" ||
+                inputKorisnickoIme.text.toString() == "" || inputLozinka.text.toString() == ""
+            ) {
+                Toast.makeText(this, "Popunite sve podatke!", Toast.LENGTH_LONG).show()
+            } else {
+                val requestQueue = Volley.newRequestQueue(this)
+
+                val jsonObject = JSONObject()
+                jsonObject.put("ime_prezime", inputImeIPrezime.text.toString())
+                jsonObject.put("email", inputEmail.text.toString())
+                jsonObject.put("korisnicko_ime", inputKorisnickoIme.text.toString())
+                jsonObject.put("lozinka", inputLozinka.text.toString())
+
+                val jsonRequest = object : JsonObjectRequest(Method.POST, url, jsonObject,
+                    Response.Listener { response ->
+                        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                    },
+                    Response.ErrorListener { error ->
+                        //error
+                    }) {
+                }
+
+                requestQueue.add(jsonRequest)
+
+                inputImeIPrezime.setText("")
+                inputEmail.setText("")
+                inputKorisnickoIme.setText("")
+                inputLozinka.setText("")
+            }
         }
     }
 }
+
