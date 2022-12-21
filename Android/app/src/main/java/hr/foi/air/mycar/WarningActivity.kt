f5b39a4ea03fa5a3d9abd73a80649239eae532e3
Forky10911 <95697055+Forky10911@users.noreply.github.com>
2022-12-21 15:49:22 +0100

    Izrađen ListView za prikaz upozorenja
    - kreirana XML datoteka listview_item.xml za uređivanje svakog elementa unutar ListViewa
    - prikaz upozorenja za vozilo

--- Android/app/src/main/java/hr/foi/air/mycar/WarningActivity.kt
+++ Android/app/src/main/java/hr/foi/air/mycar/WarningActivity.kt
@@ -3,9 +3,14 @@ package hr.foi.air.mycar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 
 class WarningActivity : AppCompatActivity() {
+
+    var array = arrayOf("Nizak tlak u gumama!", "Niska razina goriva!", "Niski tlak ulja!")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_warning)
@@ -14,6 +19,25 @@ class WarningActivity : AppCompatActivity() {
         btnClose.setOnClickListener{
             callMain()
         }
+        /*
+        // use arrayadapter and define an array
+        val arrayAdapter: ArrayAdapter<*>
+        val warnings = arrayOf(
+            "Virat Kohli", "Rohit Sharma", "Steve Smith",
+            "Kane Williamson", "Ross Taylor"
+        )
+
+        // access the listView from xml file
+        var mListView = findViewById<ListView>(R.id.warningList)
+        arrayAdapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, warnings)
+        mListView.adapter = arrayAdapter*/
+
+        val adapter = ArrayAdapter(this,
+            R.layout.listview_item, array)
+
+        val listView:ListView = findViewById(R.id.warningList)
+        listView.setAdapter(adapter)
 
     }
 
